ab74583a6a5a28bb48dc7def95d7ac0e480f1bed
Adriantorres21 <64857184+Adriantorres21@users.noreply.github.com>
2020-05-31 22:43:08 -0500

    botonModificar

--- src/Vista/ControlUser.java
+++ src/Vista/ControlUser.java
@@ -237,6 +237,7 @@ public class ControlUser extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
