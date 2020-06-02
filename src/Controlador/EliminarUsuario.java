4ea7c0b5e9a9db27080e4dc391aaca1d29b63d99
River <River@IsaacRO22>
2020-06-02 00:49:30 -0500

    Encabezados

--- src/Controlador/EliminarUsuario.java
+++ src/Controlador/EliminarUsuario.java
@@ -3,6 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+ /* 5.Patriots
+    Proyecto: SATA
+    Version: 1.1
+    01/Junio/2020
+    Caso de uso: EliminarUsuario
+Descripcion: El caso de eliminarUsuario es la clase en donde se eliminan los datos de un usuario por su id
+    El caso de uso utiliza para el funcionamiento:
+        -Número de  requerimientos: RF_01, RN_03,  RN_17,  RN_20
+        -Clase: EliminarUsuario
+        -Metodos: borrarUser
+ */
+
 package Controlador;
 
 import java.sql.Connection;
@@ -15,19 +28,21 @@ import javax.swing.JOptionPane;
  *
  * @author Mayra
  */
+
 public class EliminarUsuario {
 
     Connection conn = conexion.getConexion();
     PreparedStatement ps;
-
+    //Este metodo es para buscar el id del usuario para eliminar sus datos de la BD
+    //Número de  requerimiento: RF_03,  RN_17
     public void  borrarUser(String id) {
         try {
 
             String consulta = "DELETE FROM inicioSesion WHERE usuario = " + id;
-            Statement st;
+            Statement st; //Primero definimos la instancia para eliminar de SQL al encontrar el Id del usuario
             st = conn.createStatement();
             ps = conn.prepareStatement(consulta);
-
+            //luego se ejecuta esta consulta  y se hace la actualizacion de la BD
             ps.executeUpdate();
             
             JOptionPane.showMessageDialog(null, "Usuario eliminado exitosamente",
